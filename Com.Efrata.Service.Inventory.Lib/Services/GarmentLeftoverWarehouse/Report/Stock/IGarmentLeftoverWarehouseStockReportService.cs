c2bee8e529d7156dcd2d5b7886214a82a0b208e1
achensetyawati <achenzetya@gmail.com>
2023-11-20 16:56:13 +0700

    update add filter for report stock finished good

--- Com.Efrata.Service.Inventory.Lib/Services/GarmentLeftoverWarehouse/Report/Stock/IGarmentLeftoverWarehouseStockReportService.cs
+++ Com.Efrata.Service.Inventory.Lib/Services/GarmentLeftoverWarehouse/Report/Stock/IGarmentLeftoverWarehouseStockReportService.cs
@@ -13,7 +13,7 @@ namespace Com.Efrata.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse.Rep
         MemoryStream GenerateExcelAcc(DateTime? dateFrom, DateTime? dateTo, int unitId, int offset);
         Tuple<List<GarmentLeftoverWarehouseStockMonitoringViewModel>, int> GetMonitoringAcc(DateTime? dateFrom, DateTime? dateTo, int unitId, int page, int size, string order, int offset);
         MemoryStream GenerateExcelFinishedGood(DateTime? dateFrom, DateTime? dateTo, int unitId, int offset);
-        Tuple<List<GarmentLeftoverWarehouseStockMonitoringViewModel>, int> GetMonitoringFinishedGood(DateTime? dateFrom, DateTime? dateTo, int unitId, int page, int size, string order, int offset);
+        Tuple<List<GarmentLeftoverWarehouseStockMonitoringViewModel>, int> GetMonitoringFinishedGood(DateTime? dateFrom, DateTime? dateTo, int unitId, string ro, int page, int size, string order, int offset);
         Tuple<List<GarmentLeftoverWarehouseStockMonitoringViewModel>, int> GetMonitoringAval(DateTime? dateFrom, DateTime? dateTo, int unitId, int page, int size, string order, int offset, string typeAval);
         MemoryStream GenerateExcelAval(DateTime? dateFrom, DateTime? dateTo, int unitId, int offset, string typeAval);
     }
